6b7e406724498cd48b3ec7195ab5cd9110321889
leslie-alldridge <leslie.alldridge@gmail.com>
2021-03-21 21:18:25 +1300

    downgrade to manifest v2 for background runners

--- background.js
+++ background.js
@@ -1,1 +1,151 @@
-// TODO: Implement alarms api once it is more stable
+// Timer set up
+let TIME_LIMIT = 0;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+// Set badge color to green
+chrome.browserAction.setBadgeBackgroundColor({ color: 'green' });
+
+// Set up message listener so we can communicate with popup.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(request);
+  if (request.time) {
+    sendResponse({ data: true });
+    // Start the countdown timer
+    TIME_LIMIT = request.time;
+    startTimer(TIME_LIMIT);
+  } else if (request.getItem) {
+    console.log('getting item');
+    (async () => {
+      chrome.storage.local.get('key', (data) => {
+        console.log(data);
+        sendResponse({ data: data.key });
+      });
+    })();
+
+    return true;
+  } else if (request.reset) {
+    // reset timer
+    onTimesUp();
+    sendResponse({ data: true });
+  }
+  return true;
+});
+
+// Clears the set interval so we don't have increasing intervals on second runs
+onTimesUp = () => {
+  clearInterval(timerInterval);
+  TIME_LIMIT = 0;
+  timePassed = 0;
+  timeLeft = TIME_LIMIT;
+  timerInterval = null;
+  chrome.browserAction.setBadgeText({ text: '00:00' });
+  chrome.browserAction.setBadgeBackgroundColor({ color: 'green' });
+  chrome.storage.local.set({ key: '00:00' }, () => {
+    if (chrome.runtime.lastError) {
+      console.error(
+        'Error setting ' +
+          key +
+          ' to ' +
+          JSON.stringify(data) +
+          ': ' +
+          chrome.runtime.lastError.message
+      );
+    }
+  });
+};
+
+// Time to start the actual timer and save our state as well as update the extension badge countdown
+startTimer = (TIME_LIMIT) => {
+  // persist users time limit
+  chrome.storage.local.set({ limit: TIME_LIMIT }, () => {
+    if (chrome.runtime.lastError) {
+      console.error(
+        'Error setting ' +
+          key +
+          ' to ' +
+          JSON.stringify(data) +
+          ': ' +
+          chrome.runtime.lastError.message
+      );
+    }
+  });
+
+  // Generate warning and alert thresholds and color scheme
+  WARNING_THRESHOLD = TIME_LIMIT / 3;
+  ALERT_THRESHOLD = TIME_LIMIT / 6;
+
+  const COLOR_CODES = {
+    info: {
+      color: 'green',
+    },
+    warning: {
+      color: 'orange',
+      threshold: WARNING_THRESHOLD,
+    },
+    alert: {
+      color: 'red',
+      threshold: ALERT_THRESHOLD,
+    },
+  };
+
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+
+    chrome.browserAction.setBadgeText({ text: String(formatTime(timeLeft)) });
+
+    chrome.storage.local.set({ key: timeLeft }, () => {
+      if (chrome.runtime.lastError) {
+        console.error(
+          'Error setting ' +
+            key +
+            ' to ' +
+            JSON.stringify(data) +
+            ': ' +
+            chrome.runtime.lastError.message
+        );
+      }
+    });
+
+    setBadgeColor(timeLeft, COLOR_CODES);
+
+    if (timeLeft === 0 || timeLeft < 0) {
+      onTimesUp();
+    }
+  }, 1000);
+};
+
+// Assists with formatting between minutes and seconds
+formatTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) seconds = `0${seconds}`;
+
+  return `${minutes}:${seconds}`;
+};
+
+// Changes the extension badge color depending on our warning and alert thresholds
+setBadgeColor = (timeLeft, COLOR_CODES) => {
+  const { alert, warning } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    chrome.browserAction.setBadgeBackgroundColor({ color: 'red' });
+  } else if (timeLeft <= warning.threshold) {
+    chrome.browserAction.setBadgeBackgroundColor({ color: 'orange' });
+  }
+};
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (var key in changes) {
+    var storageChange = changes[key];
+    console.log(
+      'Storage key "%s" in namespace "%s" changed. ' +
+        'Old value was "%s", new value is "%s".',
+      key,
+      namespace,
+      storageChange.oldValue,
+      storageChange.newValue
+    );
+  }
+});
--- popup.js
+++ popup.js
@@ -9,10 +9,80 @@ let rule = document.getElementById('rule');
 
 // Timer set up
 const FULL_DASH_ARRAY = 283;
-let TIME_LIMIT = 1200;
+let TIME_LIMIT = 0;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
+
+chrome.runtime.sendMessage({ getItem: true }, function (response) {
+  console.log(response);
+  if (response.data > 0) {
+    chrome.storage.local.get('limit', (data) => {
+      console.log(data);
+      if (data) {
+        TIME_LIMIT = data.limit;
+        timePassed = TIME_LIMIT - response.data;
+        console.log(timePassed);
+      }
+    });
+    WARNING_THRESHOLD = TIME_LIMIT / 4;
+    ALERT_THRESHOLD = TIME_LIMIT / 6;
+
+    const COLOR_CODES = {
+      info: {
+        color: 'green',
+      },
+      warning: {
+        color: 'orange',
+        threshold: WARNING_THRESHOLD,
+      },
+      alert: {
+        color: 'red',
+        threshold: ALERT_THRESHOLD,
+      },
+    };
+    renderTimerHtml(response.data);
+    startTimer(COLOR_CODES);
+    // Hide start button and other fields from the landing page
+    startSessionBtn.style.display = 'none';
+    description.style.display = 'none';
+    inputDisplay.style.display = 'none';
+    title.style.display = 'none';
+    rule.style.display = 'none';
+    // Display timer options and end session button
+    sessionTimer.style.display = 'block';
+    endSession.style.visibility = 'visible';
+  } else {
+    // Timer set up
+    TIME_LIMIT = 0;
+    timePassed = 0;
+    timeLeft = TIME_LIMIT;
+    timerInterval = null;
+    onTimesUp();
+    resetToDefault();
+  }
+
+  // while (response.data > 0 && i > 0) {
+  //   console.log('in loop');
+  //   (function (i) {
+  //     setTimeout(function () {
+  //       chrome.runtime.sendMessage(
+  //         { getItem: TIME_LIMIT },
+  //         function (response) {
+  //           console.log(response);
+  //           if (response.data > 0) {
+  //             console.log(response.data);
+  //           } else {
+  //             i = 0;
+  //           }
+
+  //
+  //         }
+  //       );
+  //     }, 1000 * i);
+  //   })(i++);
+  // }
+});
 
 startSessionBtn.addEventListener('click', function () {
   // Hide start button and other fields from the landing page
@@ -28,6 +98,9 @@ startSessionBtn.addEventListener('click', function () {
 
   // Retrieve user input from the first page and convert into minutes
   TIME_LIMIT = Number(inputDisplay.value) * 60;
+  chrome.runtime.sendMessage({ time: TIME_LIMIT }, function (response) {
+    console.log(response.data);
+  });
   // Set warning and error values based on user input
 
   WARNING_THRESHOLD = TIME_LIMIT / 4;
@@ -50,7 +123,7 @@ startSessionBtn.addEventListener('click', function () {
   // Render timer and start the countdown
   renderTimerHtml(TIME_LIMIT);
 
-  startTimer(COLOR_CODES);
+  startTimer(COLOR_CODES, TIME_LIMIT);
 });
 
 endSession.addEventListener('click', () => {
@@ -60,6 +133,13 @@ endSession.addEventListener('click', () => {
 });
 
 resetToDefault = () => {
+  // Reset our background task
+  chrome.runtime.sendMessage({ reset: true }, function (response) {
+    console.log('reset background task');
+    if (response) {
+      console.log(response);
+    }
+  });
   // Change background to use our active class
   sessionTimer.style.display = 'none';
   document.body.style.backgroundColor = '#121212';
@@ -77,8 +157,8 @@ resetToDefault = () => {
   timeLeft = TIME_LIMIT;
   timerInterval = null;
   remainingPathColor = 'green';
-  chrome.action.setBadgeText({ text: '0:00' });
-  chrome.action.setBadgeBackgroundColor({ color: '#41B883' });
+  //chrome.browserAction.setBadgeText({ text: '0:00' });
+  //chrome.browserAction.setBadgeBackgroundColor({ color: '#41B883' });
   document.getElementById('base-timer-label').style.color = 'rgb(65, 184, 131)';
 };
 
@@ -124,11 +204,13 @@ function startTimer(COLOR_CODES) {
     );
 
     // Update chrome badge with countdown
-    chrome.action.setBadgeText({ text: String(formatTime(timeLeft)) });
+    //chrome.action.setBadgeText({ text: String(formatTime(timeLeft)) });
 
     setCircleDasharray();
     setRemainingPathColor(timeLeft, COLOR_CODES);
-
+    chrome.storage.local.get('key', (data) => {
+      console.log(data);
+    });
     if (timeLeft === 0) {
       onTimesUp();
     }
@@ -157,7 +239,7 @@ function setRemainingPathColor(timeLeft, COLOR_CODES) {
       .classList.add(alert.color);
 
     document.getElementById('base-timer-label').style.color = 'red';
-    chrome.action.setBadgeBackgroundColor({ color: 'red' });
+    //chrome.action.setBadgeBackgroundColor({ color: 'red' });
   } else if (timeLeft <= warning.threshold) {
     document
       .getElementById('base-timer-path-remaining')
@@ -169,7 +251,7 @@ function setRemainingPathColor(timeLeft, COLOR_CODES) {
     // change text color
     document.getElementById('base-timer-label').style.color = 'orange';
     // change badge color
-    chrome.action.setBadgeBackgroundColor({ color: 'orange' });
+    //chrome.action.setBadgeBackgroundColor({ color: 'orange' });
   }
 }
 
